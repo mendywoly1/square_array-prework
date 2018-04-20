c7eabb386c61ece2a603ba2b106f290822c665ce
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 01:09:21 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,5 +2,7 @@ def square_array(array)
   # your code here
   sqaured_array = []
   array.each do |square|
-    sqaured_array << square
+    sqaured_array << square ** 2
+  end
+  sqaured_array
 end
